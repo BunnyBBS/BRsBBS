5b787e096574f57c28267632027a83f6983ce6de
PttBBS <my1938.tw@gmail.com>
2019-03-24 16:24:01 +0800

    BRsBBS 1.6.5 Stable
    1.修正：post程式
    因應web線上發文，程式修正部份問題，自動加上檔尾資訊，並新增稿酬發放功能。
    TODO：自動加上檔頭資訊。
    2.修正：隱板警訊系統
    界面、操作優化。

--- util/post.c
+++ util/post.c
@@ -1,8 +1,48 @@
 #include "bbs.h"
 
-void keeplog(FILE *fin, char *board, char *title, char *owner) {
+static int
+payArtMoney(int uid, int money, const char *item GCC_UNUSED, ...)
+{
+    va_list ap;
+    char reason[STRLEN*3] ="";
+
+    if (!money)
+        return 0;
+
+    if (item) {
+        va_start(ap, item);
+        vsnprintf(reason, sizeof(reason)-1, item, ap);
+        va_end(ap);
+    }
+
+    int oldm, newm;
+    const char *userid;
+    time4_t     dtime = time(0);
+
+    assert(money != 0);
+    userid = getuserid(uid);
+    assert(userid);
+    assert(reason);
+
+    if (!userid)
+        return -1;
+
+    oldm = moneyof(uid);
+    newm = deumoney(uid, -money);
+
+    {
+        char buf[PATHLEN];
+        sethomefile(buf, userid, FN_RECENTPAY);
+        log_payment(buf, money, oldm, newm, reason, (int)(++dtime));
+    }
+
+    return newm;
+}
+
+void keeplog(FILE *fin, char *board, char *title, char *owner, int money, char *ip) {
     fileheader_t fhdr;
-    char genbuf[256], buf[512];
+    char genbuf[256], genbuf2[256], buf[512];
+    time4_t     dtime = time(0);
     FILE *fout;
     int bid;
     
@@ -20,16 +60,42 @@ void keeplog(FILE *fin, char *board, char *title, char *owner) {
     fclose(fin);
     fclose(fout);
 
+    if (!is_validuserid(owner)){
+        money = 0;
+    }
+#ifdef MAX_POST_MONEY
+    if (money > MAX_POST_MONEY)
+    money = MAX_POST_MONEY;
+#endif
+
     strncpy(fhdr.title, title, sizeof(fhdr.title) - 1);
     fhdr.title[sizeof(fhdr.title) - 1] = '\0';
-    
+    fhdr.multi.money = money;
     strcpy(fhdr.owner, owner);
     sprintf(genbuf, BBSHOME "/boards/%c/%s/.DIR", board[0], board);
     append_record(genbuf, &fhdr, sizeof(fhdr));
     /* XXX: bid of cache.c's getbnum starts from 1 */
     if((bid = getbnum(board)) > 0)
-	touchbtotal(bid);
+        touchbtotal(bid);
 
+    sprintf(genbuf2, BBSHOME "/boards/%c/%s/%s", board[0], board, fhdr.filename);
+    log_filef(genbuf2, LOG_CREAT,
+              "\n--\n※ 發信站: " BBSNAME "(" MYHOSTNAME "), 來自: %s\n", ip);
+#ifdef QUERY_ARTICLE_URL
+    log_filef(genbuf2, LOG_CREAT,
+              "※ " URL_DISPLAYNAME ": " URL_PREFIX "/%s\n", fhdr.filename);
+#endif
+
+    int uid;
+    if ((uid = searchuser(owner, owner)) != 0) {
+        payArtMoney(uid, -money, "%s 看板發文稿酬: %s", board, fhdr.title);
+    }
+
+    LOG_IF(LOG_CONF_POST,
+           log_filef("log/post", LOG_CREAT,
+                     "%d %s boards/%c/%s/%s %d\n",
+                     (int)(++dtime), owner, board[0], board,
+                     fhdr.filename, money));
     printf("%s", fhdr.filename);
 }
 
@@ -39,8 +105,8 @@ int main(int argc, char **argv)
 
     attach_SHM();
     resolve_boards();
-    if(argc != 5) {
-	printf("usage: %s <board name> <title> <owner> <file>\n", argv[0]);
+    if(argc != 7) {
+	printf("usage: %s <board name> <title> <owner> <file> <money> <ip>\n", argv[0]);
 	return 0;
     }
     
@@ -64,6 +130,10 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    keeplog(fp, argv[1], title, argv[3]);
+    int money = atoi(argv[5]);
+    if(money < 0)
+        money = 0;
+
+    keeplog(fp, argv[1], title, argv[3], money, argv[6]);
     return 0;
 }
